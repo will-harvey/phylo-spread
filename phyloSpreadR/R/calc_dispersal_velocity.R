bce625760643ed70a63abfd12fb50f32731cf091
Will Harvey <william.harvey@glasgow.ac.uk>
2022-07-18 17:13:13 +0100

    Function - calculate weighted/un-weighted dispersal velocity across phylogeny

---
+++ phyloSpreadR/R/calc_dispersal_velocity.R
@@ -0,0 +1,21 @@
+#' Calculate dispersal velocity
+#'
+#' By default, the un-weighted dispersal velocity which is the mean per-branch velocity
+#' averaged across all branches of the phylogeny. The weighted dispersal velocity which
+#' places greater emphasis on longer branches (in terms of time) can be returned on request.
+#'
+#' @param tree_dat dataframe with columns velocity and branch length
+#' @param weighted logical, should function return un-weighted or weighted statistic
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calc_disperal_velocity <- function(tree_dat = NA, weighted = F) {
+  if (weighted == F) {
+    mean(beast_dat$velocity, na.rm = T)
+  } else if (weighted == T) {
+    sum(beast_dat$velocity * beast_dat$branch.length, na.rm = T) /
+      sum(beast_dat$branch.length, na.rm = T)
+  }
+}
