671bc47abd149eceea7d45bb587a4a1092a6f31d
Will Harvey <william.harvey@glasgow.ac.uk>
2022-07-19 09:49:43 +0100

    Dispersal_stats deals with target_node input

--- phyloSpreadR/R/dispersal_stats.R
+++ phyloSpreadR/R/dispersal_stats.R
@@ -5,21 +5,29 @@
 #' places greater emphasis on longer branches (in terms of time) can be returned on request.
 #'
 #' @param tree_dat dataframe with columns velocity, diffusion, gcd and branch length
+#' @param target_node single clade-defining node or set of nodes can be supplied
+
 #'
 #' @return
 #' @export
 #'
 #' @examples
-dispersal_stats <- function(tree_dat = NA) {
-  velocity <- calc_dispersal_velocity(tree_dat)
-  velocity_w <- calc_dispersal_velocity(tree_dat, weighted = T)
-  diffusion <- calc_diffusion_coef(tree_dat)
-  diffusion_w <- calc_diffusion_coef(tree_dat, weighted = T)
+dispersal_stats <- function(tree_dat = NA, target_node = NULL) {
+    velocity <- calc_dispersal_velocity(tree_dat, target_node = target_node)
+    velocity_w <- calc_dispersal_velocity(tree_dat, weighted = T,
+                                          target_node = target_node)
+    diffusion <- calc_diffusion_coef(tree_dat,
+                                     target_node = target_node)
+    diffusion_w <- calc_diffusion_coef(tree_dat, weighted = T,
+                                       target_node = target_node)
+
 
   summary <- data.frame(statistic = c('Dispersal velocity',
                                       'Dispersal velocity (weighted)',
                                       'Diffusion coefficient',
                                       'Diffusion coefficient (weighted)'),
                         value = c(velocity, velocity_w, diffusion, diffusion_w))
+  summary$value <- signif(summary$value, 5)
+
   summary
 }
