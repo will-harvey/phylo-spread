7f211845c28d5b27b148edb6c1e0f09eaa14cd67
Will Harvey <william.harvey@glasgow.ac.uk>
2022-07-19 09:11:04 +0100

    function able to return stat for subset based on clade

--- phyloSpreadR/R/calc_dispersal_velocity.R
+++ phyloSpreadR/R/calc_dispersal_velocity.R
@@ -11,11 +11,46 @@
 #' @export
 #'
 #' @examples
-calc_dispersal_velocity <- function(tree_dat = NA, weighted = F) {
-  if (weighted == F) {
-    mean(beast_dat$velocity, na.rm = T)
-  } else if (weighted == T) {
-    sum(beast_dat$velocity * beast_dat$branch.length, na.rm = T) /
-      sum(beast_dat$branch.length, na.rm = T)
+calc_dispersal_velocity <- function(tree_dat = NA, weighted = F,
+                                    target_node = NULL) {
+
+  # if target node is null, calc. stat for whole tree
+  if (is.null(target_node) == T) {
+    if (weighted == F) {
+      res <- mean(tree_dat$velocity, na.rm = T)
+    } else if (weighted == T) {
+      res <- sum(tree_dat$velocity * tree_dat$branch.length, na.rm = T) /
+        sum(tree_dat$branch.length, na.rm = T)
+    }
   }
+
+  # if target_node is length == 1, get clade below node and calc. stat
+  if (length(target_node) == 1) {
+    descendants <- toolkitSeqTree::node_descendants(tree_dat,
+                                                    node_id = target_node)
+    tree_dat <- tree_dat[tree_dat$node %in% descendants,]
+
+    # calc. weighted or un-weighted stat as above but for subsetted 'tree_dat'
+    if (weighted == F) {
+      res <- mean(tree_dat$velocity, na.rm = T)
+    } else if (weighted == T) {
+      res <- sum(tree_dat$velocity * tree_dat$branch.length, na.rm = T) /
+        sum(tree_dat$branch.length, na.rm = T)
+    }
+  }
+
+  # if target_node is length > 1, calc. stat for that set of branches
+  if (length(target_node) > 1) {
+    tree_dat <- tree_dat[tree_dat$node %in% target_node,]
+
+    # calc. weighted or un-weighted stat as above but for subsetted 'tree_dat'
+    if (weighted == F) {
+      res <- mean(tree_dat$velocity, na.rm = T)
+    } else if (weighted == T) {
+      res <- sum(tree_dat$velocity * tree_dat$branch.length, na.rm = T) /
+        sum(tree_dat$branch.length, na.rm = T)
+    }
+  }
+
+  res
 }
